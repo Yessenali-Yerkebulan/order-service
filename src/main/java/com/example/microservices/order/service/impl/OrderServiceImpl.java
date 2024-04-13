192a6680e170c6a9e471448dbd81b5fff08bbff9
Yerkebulan <yerkebulanyessenali03@gmail.com>
2024-04-13 10:31:41 +0500

    add OpenFeign InventoryService Client

--- src/main/java/com/example/microservices/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/microservices/order/service/impl/OrderServiceImpl.java
@@ -1,28 +1,38 @@
 package com.example.microservices.order.service.impl;
 
+import com.example.microservices.order.client.InventoryClient;
 import com.example.microservices.order.dto.OrderRequest;
 import com.example.microservices.order.model.Order;
 import com.example.microservices.order.repository.OrderRepository;
 import com.example.microservices.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-    @Autowired
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+
+    private final InventoryClient inventoryClient;
 
     @Override
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        if(isProductInStock) {
+            Order order = new Order();
 
-        orderRepository.save(order);
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
